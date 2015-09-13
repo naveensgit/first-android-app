6d0c9ff6239c89eb41a1470260ec75819dbed8bb
naveensgit <naveenkumarselvaraj7@gmail.com>
2015-09-12 17:55:24 -0700

    Commit of all my changes for First Todo app

---
+++ app/src/main/java/com/example/naselvar/simpletodo/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.naselvar.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+    private static final int REQUEST_CODE = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String editedItemValue = data.getStringExtra("editedItemValue");
+            int editedItemPosition = data.getIntExtra("editedItemPosition", 0);
+            items.set(editedItemPosition, editedItemValue);
+            writeItems();
+            itemsAdapter.notifyDataSetChanged();
+        }
+    }
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int position, long id) {
+                        items.remove(position);
+                        writeItems();
+                        itemsAdapter.notifyDataSetChanged();
+                        return true;
+                    }
+                }
+        );
+
+        lvItems.setOnItemClickListener(
+                 new AdapterView.OnItemClickListener() {
+                     @Override
+                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                         launchComposeView(items.get(position), position);
+                     }
+                 }
+
+        );
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void launchComposeView(String itemText, int position) {
+        Intent intent = new Intent(MainActivity.this,EditItemActivity.class);
+        intent.putExtra("itemValue", itemText);
+        intent.putExtra("itemPosition", position);
+
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+}
